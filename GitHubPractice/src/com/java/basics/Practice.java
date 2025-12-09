09e6f3759411f619afe57da518a46ba80f6e6977
241Mohan <mohanmonu6779@gmail.com>
2025-12-09 07:10:03 +0530

    Name is Updated Practice.java

--- GitHubPractice/src/com/java/basics/Practice.java
+++ GitHubPractice/src/com/java/basics/Practice.java
@@ -3,7 +3,7 @@ package com.java.basics;
 public class Practice {
 	public static void main(String[] args) {
 		int a=1220;
-		String name="Shivani";
+		String name="Manasa";
 		System.out.println("Integer = "+a);
         System.out.println("Name = "+name);
 	}
